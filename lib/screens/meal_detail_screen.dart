d896c0552b2c5e107971a5db63a3125c61445c99
Michael Dadzie <dadziemikke2@gmail.com>
2021-04-03 12:14:11 +0000

    Rebuilding meal details screen ui

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -38,77 +38,106 @@ class MealDetailScreen extends StatelessWidget {
     final mealId = ModalRoute.of(context).settings.arguments as String;
     final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '${selectedMeal.title}',
-          style: TextStyle(color: Colors.red),
-        ),
-        iconTheme: new IconThemeData(color: Colors.red),
-      ),
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Image.network(
-                selectedMeal.imageUrl,
-                fit: BoxFit.cover,
-              ),
-            ),
-            buildSectionTitle(
-              context,
-              'Ingredients',
-            ),
-            buildContainer(
-              ListView.builder(
-                itemBuilder: (ctx, index) => Card(
-                    color: Colors.red,
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                      child: Text(selectedMeal.ingredients[index]),
-                    )),
-                itemCount: selectedMeal.ingredients.length,
-              ),
-            ),
-            buildSectionTitle(context, 'Steps'),
-            buildContainer(
-              ListView.builder(
-                itemBuilder: (ctx, index) => Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.red,
-                        child: Text(
-                          '#${(index + 1)}',
-                          style:
-                              TextStyle(color: Theme.of(context).accentColor),
-                        ),
-                      ),
-                      title: Text(
-                        selectedMeal.steps[index],
-                        style: TextStyle(color: Theme.of(context).accentColor),
-                      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  child: Container(
+                    height: 300,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.network(
+                      selectedMeal.imageUrl,
+                      fit: BoxFit.cover,
                     ),
-                    Divider(
-                      color: Colors.red,
-                    ),
-                  ],
+                  ),
                 ),
-                itemCount: selectedMeal.steps.length,
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 10,
-        child: Icon(isFavorite(mealId) ? Icons.star : Icons.star_border),
-        backgroundColor: Colors.red,
-        onPressed: () => toggleFavorite(mealId),
-      ),
+      // appBar: AppBar(
+      //   title: Text(
+      //     '${selectedMeal.title}',
+      //     style: TextStyle(color: Colors.red),
+      //   ),
+      //   iconTheme: new IconThemeData(color: Colors.red),
+      // ),
+      // body: SingleChildScrollView(
+      //   physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+      //   child: Column(
+      //     children: <Widget>[
+      //       Container(
+      //         height: 300,
+      //         width: double.infinity,
+      //         child: Image.network(
+      //           selectedMeal.imageUrl,
+      //           fit: BoxFit.cover,
+      //         ),
+      //       ),
+      //       buildSectionTitle(
+      //         context,
+      //         'Ingredients',
+      //       ),
+      //       buildContainer(
+      //         ListView.builder(
+      //           itemBuilder: (ctx, index) => Card(
+      //               color: Colors.red,
+      //               child: Padding(
+      //                 padding:
+      //                     EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      //                 child: Text(selectedMeal.ingredients[index]),
+      //               )),
+      //           itemCount: selectedMeal.ingredients.length,
+      //         ),
+      //       ),
+      //       buildSectionTitle(context, 'Steps'),
+      //       buildContainer(
+      //         ListView.builder(
+      //           itemBuilder: (ctx, index) => Column(
+      //             children: [
+      //               ListTile(
+      //                 leading: CircleAvatar(
+      //                   backgroundColor: Colors.red,
+      //                   child: Text(
+      //                     '#${(index + 1)}',
+      //                     style:
+      //                         TextStyle(color: Theme.of(context).accentColor),
+      //                   ),
+      //                 ),
+      //                 title: Text(
+      //                   selectedMeal.steps[index],
+      //                   style: TextStyle(color: Theme.of(context).accentColor),
+      //                 ),
+      //               ),
+      //               Divider(
+      //                 color: Colors.red,
+      //               ),
+      //             ],
+      //           ),
+      //           itemCount: selectedMeal.steps.length,
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   elevation: 10,
+      //   child: Icon(isFavorite(mealId) ? Icons.star : Icons.star_border),
+      //   backgroundColor: Colors.red,
+      //   onPressed: () => toggleFavorite(mealId),
+      // ),
     );
   }
 }
