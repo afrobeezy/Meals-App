754791ef0296e48f052627478e6f0c7b511ed309
Michael Dadzie <dadziemikke2@gmail.com>
2021-04-14 23:38:29 +0000

    Redesigned categories screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -2,27 +2,85 @@ import 'package:flutter/material.dart';
 import '../data/dummy_data.dart';
 import '../widgets/category_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
+  @override
+  _CategoriesScreenState createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      // physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-      padding: const EdgeInsets.all(25),
-      children: DUMMY_CATEGORIES
-          .map(
-            (catData) => CategoryItem(
-              catData.id,
-              catData.title,
-              catData.color,
-              catData.imageURL,
-            ),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Container(
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Hello, ',
+                        style: TextStyle(
+                            fontFamily: 'Raleway',
+                            fontSize: 30,
+                            fontWeight: FontWeight.w400,
+                            color: Theme.of(context).hoverColor),
+                        children: [
+                          TextSpan(
+                            text: 'Michael',
+                            style: TextStyle(
+                              fontFamily: 'Raleway',
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).hoverColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Container(
+                    child: Text(
+                      'What would you like to cook today ?',
+                      style: TextStyle(
+                          fontFamily: 'Raleway',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Theme.of(context).hoverColor),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 935,
+                child: ListView(
+                  padding: const EdgeInsets.all(20),
+                  physics: NeverScrollableScrollPhysics(),
+                  children: DUMMY_CATEGORIES
+                      .map(
+                        (catData) => CategoryItem(
+                          catData.id,
+                          catData.title,
+                          catData.color,
+                          catData.imageURL,
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
