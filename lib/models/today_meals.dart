06cf634b578f7350a8be5e62f1cf2a71a9f4fba0
dadzi <dadziemikke2@gmail.com>
2021-04-28 23:42:01 +0000

    Added Today's Meal model

---
+++ lib/models/today_meals.dart
@@ -0,0 +1,19 @@
+class TodayMeals {
+  String id;
+  String title;
+  String imageUrl;
+  String ingredients;
+  String duration;
+  String complexity;
+  String affordability;
+
+  TodayMeals({
+    this.id,
+    this.title,
+    this.imageUrl,
+    this.ingredients,
+    this.duration,
+    this.complexity,
+    this.affordability,
+  });
+}
