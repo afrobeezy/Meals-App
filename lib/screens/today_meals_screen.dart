a4a39832cefee8372becb804d98a623bdcae8892
dadzi <dadziemikke2@gmail.com>
2021-05-01 10:19:27 +0000

    Added details screen for today's meals

---
+++ lib/screens/today_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodayMealsScreen extends StatelessWidget {
+  final String imageUrl, title, ingredients, complexity;
+  TodayMealsScreen({
+    this.imageUrl,
+    this.title,
+    this.ingredients,
+    this.complexity,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
