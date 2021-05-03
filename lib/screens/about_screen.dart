854da7ec97c8b2c8edc0946022a2e82653edee9a
dadzi <dadziemikke2@gmail.com>
2021-05-03 20:40:31 +0000

    Added About Screen

---
+++ lib/screens/about_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class AboutScreen extends StatelessWidget {
+  static const routeName = '/about';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'About',
+          style: TextStyle(color: Theme.of(context).focusColor),
+        ),
+        iconTheme: IconThemeData(color: Theme.of(context).focusColor),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Version 1.0.0',
+                style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).hoverColor),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Build version 40218.38810201',
+                style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey[700]),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Codepush version v1.0.0',
+                style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey[700]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
