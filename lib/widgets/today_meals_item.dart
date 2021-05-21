627b8bafb445279c7d06309599331b5ca6c3c50c
dadzi <dadziemikke2@gmail.com>
2021-05-21 22:16:32 +0000

    Improve UI code

--- lib/widgets/today_meals_item.dart
+++ lib/widgets/today_meals_item.dart
@@ -6,7 +6,17 @@ import 'package:mealsapp/screens/today_meals_screen.dart';
 class TodayMealsItem extends StatelessWidget {
   final String imageUrl,
       title,
+      id,
       complexity,
+      course,
+      preptime,
+      cooktime,
+      totaltime,
+      servings,
+      calories,
+      fat,
+      protein,
+      carbo,
       step1,
       step2,
       step3,
@@ -18,7 +28,17 @@ class TodayMealsItem extends StatelessWidget {
       item4;
   const TodayMealsItem({
     this.imageUrl,
+    this.course,
+    this.preptime,
+    this.cooktime,
+    this.totaltime,
+    this.servings,
+    this.calories,
+    this.fat,
+    this.protein,
+    this.carbo,
     this.title,
+    this.id,
     this.complexity,
     this.step1,
     this.step2,
@@ -41,6 +61,16 @@ class TodayMealsItem extends StatelessWidget {
             builder: (context) => TodayMealsScreen(
               imageUrl: imageUrl,
               title: title,
+              id: id,
+              course: course,
+              preptime: preptime,
+              cooktime: cooktime,
+              totaltime: totaltime,
+              servings: servings,
+              calories: calories,
+              fat: fat,
+              protein: protein,
+              carbo: carbo,
               complexity: complexity,
               step1: step1,
               step2: step2,
@@ -55,92 +85,120 @@ class TodayMealsItem extends StatelessWidget {
           ),
         );
       },
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(15),
-        child: Container(
-          child: Stack(
-            children: [
-              Hero(
-                tag: title,
-                child: FadeInImage(
-                  placeholder: AssetImage('assets/load/load.gif'),
-                  fadeInDuration: Duration(milliseconds: 300),
-                  image: CachedNetworkImageProvider(imageUrl),
-                  fit: BoxFit.cover,
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                ),
-              ),
-              Container(
-                color: Colors.black26,
-              ),
-              Positioned(
-                top: 10,
-                left: 10,
-                right: 30,
-                child: Container(
-                  height: 20,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 5, right: 5, top: 2, bottom: 2),
-                    child: Center(
-                      child: Text(
-                        complexity,
-                        style: TextStyle(
-                          fontFamily: 'Raleway',
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Colors.black54,
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Container(
+                height: 170,
+                child: Hero(
+                  tag: title,
+                  child: FadeInImage(
+                    placeholder: AssetImage('assets/load/load.gif'),
+                    fadeInDuration: Duration(milliseconds: 300),
+                    image: CachedNetworkImageProvider(imageUrl),
+                    fit: BoxFit.cover,
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
                   ),
                 ),
               ),
-              Positioned(
-                bottom: 10,
-                left: 10,
-                right: 20,
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: TextStyle(
-                          fontFamily: 'Raleway',
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        'ingredients',
-                        style: TextStyle(
-                          fontFamily: 'Raleway',
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey[300],
-                        ),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
-                  ),
-                ),
+            ),
+            SizedBox(height: 3.5),
+            Text(
+              title,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).hoverColor,
               ),
-            ],
-          ),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(height: 3.5),
+            Row(
+              children: [
+                Text(
+                  course,
+                  style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey[700],
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  ' • ',
+                  style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey[700],
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  totaltime,
+                  style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey[700],
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ],
         ),
+        // child: Stack(
+        //   children: [
+        //     Hero(
+        //       tag: title,
+        //       child: FadeInImage(
+        //         placeholder: AssetImage('assets/load/load.gif'),
+        //         fadeInDuration: Duration(milliseconds: 300),
+        //         image: CachedNetworkImageProvider(imageUrl),
+        //         fit: BoxFit.cover,
+        //         height: MediaQuery.of(context).size.height,
+        //         width: MediaQuery.of(context).size.width,
+        //       ),
+        //     ),
+        //     Container(
+        //       color: Colors.black12,
+        //     ),
+        //     Positioned(
+        //       bottom: 10,
+        //       left: 10,
+        //       right: 20,
+        //       child: Container(
+        //         child: Column(
+        //           crossAxisAlignment: CrossAxisAlignment.start,
+        //           children: [
+        //             Text(
+        //               title,
+        //               style: TextStyle(
+        //                 fontFamily: 'Raleway',
+        //                 fontSize: 15,
+        //                 fontWeight: FontWeight.bold,
+        //                 color: Colors.white,
+        //               ),
+        //               maxLines: 2,
+        //               overflow: TextOverflow.ellipsis,
+        //             ),
+        //           ],
+        //         ),
+        //       ),
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
